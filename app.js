175f19146efea05ef4b11d4b8629a95a956f38ab
Ngawang C <ngawangchoeying303@gmail.com>
2022-05-09 20:44:08 +0530

    created a txt file since it was done in console

---
+++ app.js
@@ -0,0 +1,6 @@
+//String.prototype is a "template object" for every single string.
+//We could go crazy and add our own method called yell...
+String.prototype.yell = function() {
+	return `OMG!!! ${this.toUpperCase()}!!!!! AGHGHGHG!`;
+};
+
